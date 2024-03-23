b9c1bccdd694498087bd9126573d8bbcc293145b
vadim.job1 <95861926+jongold9@users.noreply.github.com>
2024-03-23 08:45:45 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,19 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 application = Flask(__name__)
 
-
-@application.route("/")
-def root():
-    return render_template("index.html")
-
-
-@application.route("/hello")
+@application.route('/')
 def index():
-    return "Hello World from Flask Hello Page.<b> v1.0"
+    ip_address = request.remote_addr
+    return render_template('index.html', ip_address=ip_address)
 
-#--------Main------------------
-if __name__ == "__main__":
-    application.debug = True
-    application.run()
-#------------------------------
+if __name__ == '__main__':
+    application.run(debug=True)
