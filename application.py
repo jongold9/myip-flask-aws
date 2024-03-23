d10890c6d44adc0d5d0abf5d463ee808ad19908a
vadim.job1 <95861926+jongold9@users.noreply.github.com>
2024-03-23 09:20:22 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
+import requests
 
 application = Flask(__name__)
 
 @application.route('/')
 def index():
-    # Получение IP-адреса посетителя
-    ip_address = request.headers.get('X-Forwarded-For')
-    return render_template('index.html', ip_address=ip_address)
+    external_ip = requests.get('https://api.ipify.org').text
+    internal_ip = request.host.split(':')[0]
+    user_external_ip = request.remote_addr
+    user_internal_ip = request.access_route[0]
+
+    return render_template('index.html', external_ip=external_ip, internal_ip=internal_ip,
+                           user_external_ip=user_external_ip, user_internal_ip=user_internal_ip)
 
 if __name__ == '__main__':
     application.run(debug=True)
