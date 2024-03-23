4c421f33b81ba69a6facbb428b92d32e381a25a9
vadim.job1 <95861926+jongold9@users.noreply.github.com>
2024-03-23 08:55:23 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,19 @@
+import requests
 from flask import Flask, render_template, request
 
 application = Flask(__name__)
 
+def get_external_ip():
+    try:
+        response = requests.get('https://api.ipify.org?format=json')
+        return response.json()['ip']
+    except Exception as e:
+        print("Error getting external IP:", e)
+        return None
+
 @application.route('/')
 def index():
-    ip_address = request.remote_addr
+    ip_address = get_external_ip()
     return render_template('index.html', ip_address=ip_address)
 
 if __name__ == '__main__':
